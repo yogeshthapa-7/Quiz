3afb92194d62e4ec7a75b5fc9fb8f7d0aca37dbc
Yogesh Thapa <161129686+yogeshthapa-7@users.noreply.github.com>
2026-01-07 07:50:46 +0545

    Create QuizQuestion

---
+++ data/QuizQuestion.ts
@@ -0,0 +1,44 @@
+export const quizQuestionsAnswers = [
+    {
+        id: 0,
+        question: "What is the capital of France?",
+        answers: ["Berlin", "Madrid", "Paris", "Rome"],
+        correctAnswer: "Paris"
+    },
+    {
+        id: 1,
+        question: "Which planet is known as the Red Planet?",
+        answers: ["Earth", "Mars", "Jupiter", "Saturn"],
+        correctAnswer: "Mars"
+    },
+    {
+        id: 2,
+        question: "What is the largest ocean on Earth?",
+        answers: ["Atlantic Ocean", "Indian Ocean", "Arctic Ocean", "Pacific Ocean"],
+        correctAnswer: "Pacific Ocean"
+    },
+    {
+        id: 3,
+        question: "Who wrote 'Romeo and Juliet'?",
+        answers: ["Charles Dickens", "William Shakespeare", "Mark Twain", "Jane Austen"],
+        correctAnswer: "William Shakespeare"
+    },
+    {
+        id: 4,
+        question: "What is the chemical symbol for Gold?",
+        answers: ["Go", "Gd", "Au", "Ag"],
+        correctAnswer: "Au"
+    },
+    {
+        id: 5,
+        question: "Which country hosted the 2016 Summer Olympics?",
+        answers: ["China", "Brazil", "UK", "Russia"],
+        correctAnswer: "Brazil"
+    },
+    {
+        id: 6,
+        question: "What is the hardest natural substance on Earth?",
+        answers: ["Gold", "Iron", "Diamond", "Silver"],
+        correctAnswer: "Diamond"
+    }
+]
